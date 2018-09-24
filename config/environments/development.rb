a55d3c0f95daaca1b922481ae3a69ab47662710c
wasiotakahiro <wasiotw@yahoo.co.jp>
2018-09-24 19:28:53 +0900

    erropage

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener
   # config.action_mailer.delivery_method = :letter_opener_web
+  config.consider_all_requests_local = false
   end
--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,6 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
-  resources :contacts
 
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
