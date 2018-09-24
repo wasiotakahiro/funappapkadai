9fe7843af3a667090b178d15e5327ec12f137b18
wasiotakahiro <wasiotw@yahoo.co.jp>
2018-09-24 20:08:13 +0900

    retouch mailer

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,5 @@ Rails.application.routes.draw do
   end
 
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+   mount LetterOpenerWeb::Engine, at: "/letter_opener"
 end
