b08197864f1f4934e178755c9a16b95aeac5793f
wasiotakahiro <wasiotw@yahoo.co.jp>
2018-09-24 19:36:17 +0900

    dev retouch mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :letter_opener_web
   config.consider_all_requests_local = false
   end
