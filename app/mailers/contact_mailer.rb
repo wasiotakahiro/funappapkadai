d878b7018e8acdb59e2bf414d5d1d7ac9f8d7c77
wasiotakahiro <wasiotw@yahoo.co.jp>
2018-09-24 14:58:53 +0900

    css retouch2”

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ApplicationMailer
     def contact_mail(reservation)
       @reservation = reservation
-      #@url = 'https://sheltered-scrubland-71446.herokuapp.com/'
+      @url = 'https://hidden-everglades-54590.herokuapp.com/'
         mail to: @reservation.email, subject: '投稿しました'
     end
   end
