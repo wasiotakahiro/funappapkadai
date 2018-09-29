84a1773dc35c796ff85a6682939fcf66e5ea227c
wasiotakahiro <wasiotw@yahoo.co.jp>
2018-09-29 21:21:33 +0900

    提出課題

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Funappap
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
-    I18n.reload!
+    i18n.reload!
     config.i18n.reload!
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
