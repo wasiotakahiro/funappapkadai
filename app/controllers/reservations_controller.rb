8ef03b5ff3478b3ecf4b64238e5c73625b2901d3
wasiotakahiro <wasiotw@yahoo.co.jp>
2018-09-29 21:03:45 +0900

    提出課題

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -63,8 +63,8 @@ class ReservationsController < ApplicationController
 
   def mail
     @reservation = Reservation.find(params[:reservation_id].to_i)
-    ContactMailer.contact_mail(@reservation).deliver  ##追記
-    redirect_to reservation_path(params[:reservation_id])
+    ContactMailer.contact_mail(@reservation).deliver
+    redirect_to reservations_path(params[:reservation_id]), notice: "メールを送信しました！"
   end
 
   private
